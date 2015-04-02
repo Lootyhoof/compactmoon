1ce0d123759acdb4678538fb25f56b83c6316404
Lootyhoof <lootyhoofer@gmail.com>
2015-04-02 13:03:49 +0100

    Update locales

--- src/options/defaults/preferences/classiccompactoptions.js
+++ src/options/defaults/preferences/classiccompactoptions.js
@@ -24,8 +24,6 @@ pref("extensions.compactmoonoptions.toHowCompactCustomBar", 0);
 pref("extensions.compactmoonoptions.toHowCompactTabBar", 0);
 pref("extensions.compactmoonoptions.toHowCompactAddonBar", 0);
 
-pref("extensions.compactmoonoptions.toFirstRun", false);
-
 
 // See http://kb.mozillazine.org/Localize_extension_descriptions
 pref("extensions.{ff497972-c067-44d8-b98e-98e62085837f}.description", "chrome://compactmoonoptions/locale/classiccompactoptions.properties");
